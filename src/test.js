de79d6d0e63e9d5da81de6d3cbcb0890f6767b60
Erez Sobel <sobelerez@gmail.com>
2018-10-19 16:52:08 +0200

    test(tests): remove snapshot testing

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NumPad from './'
-import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
 
 const props = {
   handleChange: jest.fn()
@@ -8,26 +8,7 @@ const props = {
 
 describe('NumPad', () => {
   it('renders NumPad correctly', () => {
-    const component = renderer.create(<NumPad {...props} />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-
-  it('renders NumPad correctly in disabled mode', () => {
-    const component = renderer.create(<NumPad {...props} disabled />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-
-  it('calls function when a button is clicked', () => {
-    const wrapper = mount(<NumPad {...props} />)
-    const button7 = wrapper.find('span').at(2)
-
-    expect(props.handleChange).toHaveBeenCalledTimes(0)
-
-    button7.simulate('click')
-
-    expect(toJson(wrapper)).toMatchSnapshot()
-    expect(props.handleChange).toHaveBeenCalledTimes(1)
+    const component = mount(<NumPad {...props} />)
+    expect(component).toHaveLength(1)
   })
 })
